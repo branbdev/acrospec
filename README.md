8902736280d55b5f684ae4d0b0f8e638d73f4c26
Brandon Bowen <aghoraphobic@gmail.com>
2023-04-04 00:10:09 -0700

    Added Django Rest API and React frontend

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is a Readme
+
+For Grinding and Threading Manufacturer
---
+++ backend/base/products.py
@@ -0,0 +1,74 @@
+products = [
+    {
+        '_id': '1',
+        'name': 'Cam-Out In Feed',
+        'image': '',
+        'description':
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis viverra nibh cras pulvinar.',
+        'category': 'Centerless',
+        'diameter': 1/8,
+        'isMetric': False,
+        'price': 899.99,
+        'countInStock': 10,
+    },
+    {
+        '_id': '2',
+        'name': 'iPhone 11 Pro 256GB Memory',
+        'image': '',
+        'description':
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum sed arcu non odio euismod.',
+        'category': 'Knurling',
+        'diameter': 100,
+        'isMetric': True,
+        'price': 299.99,
+        'countInStock': 0,
+    },
+    {
+        '_id': '3',
+        'name': 'Flat Die',
+        'image': '',
+        'description':
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis tellus in metus vulputate eu scelerisque.',
+        'category': 'Thread Rolling',
+        'diameter': 7/8,
+        'isMetric': False,
+        'price': 49.99,
+        'countInStock': 5,
+    },
+    {
+        '_id': '4',
+        'name': 'In Feed',
+        'image': '',
+        'description':
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit pellentesque habitant morbi tristique senectus et.',
+        'category': 'Centerless',
+        'diameter': 5,
+        'isMetric': True,
+        'price': 1049.99,
+        'countInStock': 11,
+    },
+    {
+        '_id': '5',
+        'name': 'Logitech G-Series Gaming Mouse',
+        'image': '',
+        'description':
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium quam vulputate dignissim suspendisse.',
+        'category': 'Knurling',
+        'diameter': 5/8,
+        'isMetric': False,
+        'price': 149.99,
+        'countInStock': 7,
+    },
+    {
+        '_id': '6',
+        'name': 'Thru Feed',
+        'image': '',
+        'description':
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh mauris cursus mattis molestie a.',
+        'category': 'Centerless',
+        'diameter': 3.5,
+        'isMetric': True,
+        'price': 99.99,
+        'countInStock': 0,
+    },
+]
